1ae7baed6b21636beef8c63e39a9b88a31cc2d5a
Paul Zellmer <paulzellmer9@gmail.com>
2014-11-07 18:49:58 -0800

    js fix

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -3,76 +3,76 @@ function Login(){
 		setTimeout(function(){
     	$(".accordion-navigation .content").removeClass('active')
       $("#panel2").addClass('active')
-		}, 250)	
+		}, 100)	
 		setTimeout(function(){
 			$("input[name='email']").val('u')
-		}, 500)
+		}, 200)
 		setTimeout(function(){
 			$("input[name='email']").val('us')
-		}, 750)
+		}, 300)
 		setTimeout(function(){
 			$("input[name='email']").val('use')
-		}, 1000)
+		}, 400)
 		setTimeout(function(){
 			$("input[name='email']").val('user')
-		}, 1250)
+		}, 500)
 		setTimeout(function(){
 			$("input[name='email']").val('user@')
-		}, 1500)
+		}, 600)
 		setTimeout(function(){
 			$("input[name='email']").val('user@g')
-		}, 1750)
+		}, 700)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gm')
-		}, 2000)
+		}, 800)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gma')
-		}, 2250)
+		}, 900)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmai')
-		}, 2500)
+		}, 1000)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail')
-		}, 2750)
+		}, 1100)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail')
-		}, 3000)
+		}, 1200)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail.')
-		}, 3250)
+		}, 1300)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail.c')
-		}, 3500)
+		}, 1400)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail.co')
-		}, 3750)
+		}, 1500)
 		setTimeout(function(){
 			$("input[name='email']").val('user@gmail.com')
-		}, 4000)
+		}, 1600)
 		setTimeout(function(){
 			$("input[name='password']").val('Ja')
-		}, 4250)
+		}, 1700)
 		setTimeout(function(){
 			$("input[name='password']").val('Ja')
-		}, 4500)
+		}, 1800)
 		setTimeout(function(){
 			$("input[name='password']").val('JaV')
-		}, 4750)
+		}, 1900)
 		setTimeout(function(){
 			$("input[name='password']").val('JaVa')
-		}, 5000)
+		}, 2000)
 		setTimeout(function(){
 			$("input[name='password']").val('JaVa9')
-		}, 5250)
+		}, 2100)
 		setTimeout(function(){
 			$("input[name='password']").val('JaVa98')
-		}, 5500)
+		}, 2200)
 		setTimeout(function(){
 			$("input[name='password']").val('JaVa981')
-		}, 5750)
+		}, 2300)
 		setTimeout(function(){
 			$("#submit").trigger('click')
-		}, 6000)	
+		}, 2400)	
 	}
 }
 Login();
